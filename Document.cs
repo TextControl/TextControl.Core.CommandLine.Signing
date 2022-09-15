db1a665eed777f5c731d17bee370d9b12cff4afd
Bjoern Meyer <bjoern@textcontrol.com>
2022-09-15 20:11:38 +0200

    Add project files.

---
+++ Document.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TXTextControl;
+
+namespace tx_sign_console {
+	public class Document {
+		public static void SignDocument() {
+
+			using (ServerTextControl tx = new ServerTextControl()) {
+
+				tx.Create();
+
+				// add dummy text
+				tx.Text = "This is a signed document";
+
+				SaveSettings saveSettings = new SaveSettings();
+
+				// add digital signature to sign complete document
+				saveSettings.DigitalSignature = new DigitalSignature(
+					new System.Security.Cryptography.X509Certificates.X509Certificate2("textcontrolself.pfx", "123"), null);
+
+				// export to PDF
+				tx.Save("signed_documents.pdf", StreamType.AdobePDF, saveSettings);
+			}
+
+		}
+
+		public static void SignFields() {
+			using (ServerTextControl tx = new ServerTextControl()) {
+
+				tx.Create();
+
+				// create a signature field
+				SignatureField signatureField = new SignatureField(
+					new System.Drawing.Size(2000, 2000), "txsign", 10);
+
+				// set image representation
+				signatureField.Image = new SignatureImage("signature.svg", 0);
+
+				// insert the field
+				tx.SignatureFields.Add(signatureField, -1);
+
+				// create a digital signature (for each field, if required)
+				DigitalSignature digitalSignature = new DigitalSignature(
+					new System.Security.Cryptography.X509Certificates.X509Certificate2(
+						"textcontrolself.pfx", "123"), null, "txsign");
+
+				// apply the signatures to the SaveSettings
+				SaveSettings saveSettings = new SaveSettings() {
+					SignatureFields = new DigitalSignature[] { digitalSignature }
+				};
+
+				// export to PDF
+				tx.Save("signed_fields.pdf", StreamType.AdobePDF, saveSettings);
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using tx_sign_console;
+using TXTextControl;
+
+Document.SignDocument();
+Document.SignFields();
+
+Console.WriteLine("Documents saved!");
